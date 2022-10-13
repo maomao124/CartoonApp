510f80354fff8adf6d6613fcd3dd5e6ecb90bf8f
mao <1296193245@qq.com>
2022-10-13 14:06:47 +0800

    CartoonApp

---
+++ app/src/main/java/mao/cartoonapp/CartoonHistoryActivity.java
@@ -0,0 +1,18 @@
+package mao.cartoonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CartoonHistoryActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartoon_history);
+
+
+    }
+}
