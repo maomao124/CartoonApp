e12bdae1c6de776cb9aea30721186bc8ba02f1d6
mao <1296193245@qq.com>
2022-10-26 14:53:56 +0800

    CartoonApp

--- app/src/main/java/mao/cartoonapp/ContentActivity.java
+++ app/src/main/java/mao/cartoonapp/ContentActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -231,6 +232,13 @@ public class ContentActivity extends AppCompatActivity
     @Override
     public void onWindowFocusChanged(boolean hasFocus)
     {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+        {
+            WindowManager.LayoutParams layoutParams = getWindow().getAttributes();
+            layoutParams.layoutInDisplayCutoutMode =
+                    WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+            getWindow().setAttributes(layoutParams);
+        }
         getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
