260804d466a7fb1bc2d9c70e737a6b3cc9360a00
mao <1296193245@qq.com>
2022-10-26 13:50:50 +0800

    CartoonApp

--- app/src/main/java/mao/cartoonapp/ContentActivity.java
+++ app/src/main/java/mao/cartoonapp/ContentActivity.java
@@ -8,9 +8,12 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
@@ -43,8 +46,8 @@ public class ContentActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_content);
-
         Intent intent = getIntent();
         if (intent == null)
         {
@@ -153,6 +156,7 @@ public class ContentActivity extends AppCompatActivity
                 }
             }
 
+
             @Override
             public void onLoadResource(WebView view, String url)
             {
@@ -223,6 +227,19 @@ public class ContentActivity extends AppCompatActivity
         webView.loadUrl(html);
     }
 
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus)
+    {
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -251,6 +268,28 @@ public class ContentActivity extends AppCompatActivity
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        menu.add(1, 1, 1, "刷新");
+        menu.add(1, 2, 2, "页面返回");
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        registerForContextMenu(webView);
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        unregisterForContextMenu(webView);
     }
 
     /**
